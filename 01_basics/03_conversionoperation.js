61da96d44c9fce905d42cf023f20b2a741a30f93
utkarshsingh7784 <140692187+utkarshsingh7784@users.noreply.github.com>
2024-06-22 11:52:00 +0000

    conversion of data type

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,50 @@
+let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+/*let score = null
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+// let score = undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => nan
+// true => 1  false => 0
+
+let isLoggedIn = "hello"
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+
+// 1 => true  0=> false
+// "" => false
+// "hello" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
