3ae34b6596b9ba90badd15eb0bdebbab32da9955
utkarshsingh7784 <140692187+utkarshsingh7784@users.noreply.github.com>
2024-06-23 07:51:59 +0000

    math and nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+const score =  100
+// console.log(score);
+
+const balance = new Number(400)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const anotherNum = 123.8966
+// console.log(anotherNum.toPrecision(3));
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+// ----------------math----------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.9));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(3.2));
+// console.log(Math.min(3,4,5,6));
+// console.log(Math.max(3,4,5,6));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min +1))+ min);
