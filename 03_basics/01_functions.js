0a7d0bd63d5dd467f9c7ddc67e8ec7e602e0a5d3
utkarshsingh7784 <140692187+utkarshsingh7784@users.noreply.github.com>
2024-06-25 10:31:10 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,39 @@
+// function addTwoNumbers(number1,number2){
+//     console.log(number1 + number2);
+// }
+// const result = addTwoNumbers(3,5)
+// console.log("Result: ", result);  //check value inside result
+
+//method-2
+// function addTwoNumbers(number1,number2){
+//     // let result = number1 + number2
+//     // return result
+
+//        //or
+
+//        return number1 + number2
+
+// }
+// const result = addTwoNumbers(3,5)
+// console.log("Result: ", result);
+
+
+//method-3
+// function loginUserMessage(username){
+//     return `${username} just logged in`
+// }
+// // console.log(loginUserMessage("utkarsh"));
+// console.log(loginUserMessage());
+
+
+//method-4
+// function loginUserMessage(username){
+//     if(username === undefined){
+//    console.log("please enter a user name");
+//     return
+//     }
+//     return `${username} just logged in`
+// }
+// // console.log(loginUserMessage("amit"));
+// console.log(loginUserMessage());
+
