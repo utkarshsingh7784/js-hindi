f477a462205fbabc1c9bfcd26bc6e69c48523351
utkarshsingh7784 <140692187+utkarshsingh7784@users.noreply.github.com>
2024-06-25 07:49:21 +0000

    object1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,50 @@
+// singleton means- koi bhi constructor se jab aap banate hai toh singleton ek object banta hai.
+//yaani ye apni tarike ka ek hi objet hai.
+
+// object.create - isi ko bolte hai  constructor method. isi ke andar singleton banta hai
+
+
+//object literals
+
+const muSym = Symbol("key1") //symbol ko object ke inside add karna
+
+const jsUser = {
+    name:"utkarsh",
+    "full name": "utkarsh singh",
+    [muSym]:"mykey1",
+    age:20,
+    location:"jaipur",
+    email:"yahoo@google.com",
+    isLoggedIn:false,
+    lastLoginDays:["monday","friday"]
+}
+// console.log(jsUser.name);
+// console.log(jsUser["name"]);
+// console.log(jsUser["full name"]);
+// console.log( jsUser[muSym]);
+
+// jsUser.email = "yahoo@chatgpt.com" //value overwrite
+//   Object.freeze(jsUser)           //object-freeze method
+//   jsUser.email  = "anc@gmail.com"
+//   console.log(jsUser);
+
+   jsUser.greeting = function(){    //function inside object
+    console.log("hello js user");
+   }   
+//    console.log(jsUser.greeting);   //function execute nhi hua. only function ka reference aaya hai 
+   
+               
+
+jsUser.greetingTwo = function(){           //object ke inside value ko reference karna
+    console.log(`hello js user, ${this.name} `);  //same object ko reference karne ke liye this
+   }                                     
+  console.log(jsUser.greeting());
+  console.log(jsUser.greetingTwo());
+
+
+
+
+
+
+
+
