28131703276ee560334a129635676c7a917a1a47
utkarshsingh7784 <140692187+utkarshsingh7784@users.noreply.github.com>
2024-06-25 06:03:36 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,36 @@
+const myArr = [1,2,3,4,5]
+const heros = ["shaktimaan","ironman","spiderman"]
+const myArr2 = new Array(6,7,8,9,0,1)
+// console.log(myArr[0]);
+// console.log(myArr2[4]);
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(5));
+// console.log(myArr.lastIndexOf(5));
+
+// const newArray = myArr.join()
+// console.log(myArr);
+// console.log(  newArray);
+// console.log(typeof newArray);
+
+
+console.log("a",myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+
+console.log("b",myArr);
+
+
+const myn2 = myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myn2);
+
