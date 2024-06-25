dc95c99ef3dd5f41d53cf988fce98d698f381604
utkarshsingh7784 <140692187+utkarshsingh7784@users.noreply.github.com>
2024-06-25 06:39:09 +0000

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,21 @@
+const marvel_hreos = ["ironman","spiderman","thor","hulk"]
+const dc_heros =["superman","batman","flash"]
+
+// marvel_hreos.push(dc_heros)
+// console.log(marvel_hreos);
+// console.log(marvel_hreos[4][1]);
+
+// const allHeros = marvel_hreos.concat(dc_heros)
+// console.log(allHeros);
+
+// const all_heros = [...marvel_hreos,...dc_heros]
+// console.log(all_heros);
+
+// const another_array = [1,2,3,[4,5],6,7,8,[1,2,3,4,[5,6,7]]];
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+console.log(Array.isArray("gaurav"));
+console.log(Array.from("gaurav"));
+
+console.log(Array.from({name:"gaurav"})); //intersting case
