308a16d58e4fb009dea8e36f9cdefd09a5b1ee7d
utkarshsingh7784 <140692187+utkarshsingh7784@users.noreply.github.com>
2024-06-28 10:11:39 +0000

    seven

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -2,7 +2,18 @@ const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 
 // const newNum = myNumbers.map( (num) => num +10 )
 //or
-const newNum = myNumbers.map( (num) => {
-    return num + 10
-} )
+// const newNum = myNumbers.map( (num) => {
+//     return num + 10
+// } )
+
+
+
+//chaining
+
+const newNum = myNumbers
+                    .map( (num) => num * 10)
+                    .map( (num) => num + 1)
+                    .filter( (num) => num >= 40)
+
+
 console.log(newNum);
