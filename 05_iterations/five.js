49d0ecdc5551c75b70ed486eb3228d4ff94d4847
utkarshsingh7784 <140692187+utkarshsingh7784@users.noreply.github.com>
2024-06-28 08:06:35 +0000

    array

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,3 +1,47 @@
 const coding  =["python","javascript","cpp","css","html"]
 
-coding.for
+//function
+// coding.forEach( function (val) {
+//     console.log(val);
+// })
+
+//arrow function
+// coding.forEach( (item) => {
+// console.log(item);
+// })
+
+//other method
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+
+//other method
+coding.forEach( (item,index,arr)=> {
+console.log(item,index,arr);
+})
+
+// this type - [{},{},{}]   (object inside array)
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languageFileName : "js"
+
+    },
+    {
+        languageName : "java",
+        languageFileName : "java"
+
+    },
+    {
+        languageName : "python",
+        languageFileName : "py"
+
+    }
+]
+myCoding.forEach( (item) => {
+  console.log(item.languageFileName);
+})
