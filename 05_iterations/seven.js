e74725d67ad2d4721e7132c63e3d7a720f93e22b
utkarshsingh7784 <140692187+utkarshsingh7784@users.noreply.github.com>
2024-06-28 09:49:42 +0000

    helllo

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,4 +1,8 @@
 const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 
-const newNum = myNumbers.map( (num) => num +10 )
+// const newNum = myNumbers.map( (num) => num +10 )
+//or
+const newNum = myNumbers.map( (num) => {
+    return num + 10
+} )
 console.log(newNum);
