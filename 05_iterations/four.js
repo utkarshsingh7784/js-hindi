ee49a894ebc3c278644fbacb9f4847a5ba24e3b7
utkarshsingh7784 <140692187+utkarshsingh7784@users.noreply.github.com>
2024-06-28 07:27:16 +0000

    object

---
+++ 05_iterations/four.js
@@ -0,0 +1,22 @@
+const myObj = {
+    js : 'javascript',
+    ruby : 'ruby',
+    py : "python",
+    swift : "swift by apple"
+}
+
+    for (const key in myObj) {
+       console.log(`${key} shortcut for ${myObj[key]}`);
+    }
+
+    //for object print we use for in loop
+
+    //what we can print array from for in loop
+const programming = ["js","ruby","py","java","cpp"]
+
+for
+ 
+
+
+
+    //key print karne se arrays ki key aayi hai
