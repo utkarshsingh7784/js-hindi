0b6eba0f06ec86e18ff03d7031ec0773d1d2d19a
utkarshsingh7784 <140692187+utkarshsingh7784@users.noreply.github.com>
2024-07-03 08:21:27 +0000

    promise

--- 11_advance_one/promise.js
+++ 11_advance_one/promise.js
@@ -80,3 +80,27 @@ async function consumePromiseFive(){
     }
 }
 consumePromiseFive()
+
+
+// try-cath
+// async function getAllUsers(){
+//  try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+//     console.log(data);
+//  } catch (error) {
+//     console.log("E: ", error);
+//  }
+// }
+// getAllUsers()
+
+//if we want to write same code from then and catch
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then( (response) => {
+      return response.json()
+})
+.then( (data) =>{
+   console.log(data);
+})
+.catch((error) => console.log(error))
